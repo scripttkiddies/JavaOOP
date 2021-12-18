c7e57136d064b6b255b36d080f5821618698b298
Scriptt Kiddies Project <57934376+scripttkiddies@users.noreply.github.com>
2021-12-18 14:34:03 +0700

    Create For.java

---
+++ For.java
@@ -0,0 +1,14 @@
+1-10
+  public static void main(String[] args){
+  for(int i=1; i<11; i++){
+    System.out.println(i);
+  }
+}
+
+10-1
+  public static void main(String[] args){
+  for(int i=11; i>0; i--){
+    System.out.println(i);
+  }
+} 
+  
