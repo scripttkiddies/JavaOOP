3a23ec7ed307f62c3f0422f01af7b00686888595
Scriptt Kiddies Project <57934376+scripttkiddies@users.noreply.github.com>
2022-01-08 14:53:26 +0700

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,18 @@
+public static void main(String[] args){
+        
+        String[] kota ={"Jakarta", "Surabaya", "Semarang", "Bandung"};
+
+		System.out.println("menampilkan elemen array dengan satu2");  
+	    		
+                        System.out.println(kota[0]);
+	    		System.out.println(kota[1]);
+	    		System.out.println(kota[2]);
+			System.out.println(kota[3]);
+                        
+               
+                        
+                      // menampilkan elemen array dengan for next
+                        System.out.println("menampilkan elemen array dengan for next");  
+			for(int i=0; i<kota.length; i++)
+				System.out.println(kota[i]);  
+	 	}
